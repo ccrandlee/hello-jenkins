8c15a9a8cd9fda75f517e508800607db6fa39d68
ccrandlee <xuelwang@tibco-support.com>
2015-05-23 17:37:45 +0800

    Update app.js

--- app.js
+++ app.js
@@ -6,6 +6,6 @@ app.get('/', function (req, res) {
   res.send('hello world');
 });
  
-app.listen(process.env.OPENSHIFT_NODEJS_PORT || 5000);
+app.listen(process.env.OPENSHIFT_NODEJS_PORT || 80);
  
 module.exports = app;
