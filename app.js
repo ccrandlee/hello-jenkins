62e604b6b79d7e8f466a39a2d8f4c413f5e30c6e
ccrandlee <xuelwang@tibco-support.com>
2015-05-25 18:25:16 -0700

    Switch main003 to hello jenkins

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
  
 app.get('/', function (req, res) {
-  res.send('hello jenkins02');
+  res.send('hello jenkins');
 });
  
 app.listen(process.env.OPENSHIFT_NODEJS_PORT || 80);
